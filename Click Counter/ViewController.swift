2fa2dcdc69d23258837cd47d0ec340898a3bd286
Paul Campbell <paulcampbell@pcampbellHLlaptop.local>
2016-08-10 14:44:45 -0500

    hand code button

--- Click Counter/ViewController.swift
+++ Click Counter/ViewController.swift
@@ -13,11 +13,23 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        //Label
         var label = UILabel()
         label.frame = CGRectMake(150,150,60,60)
         label.text = "0"
         
         self.view.addSubview(label)
+        
+        //Button
+        var button = UIButton()
+        button.frame =  CGRectMake(150,250,60,60)
+        button.setTitle("Click", forState: .Normal)
+        button.setTitleColor(UIColor.blueColor(), forState: .Normal)
+        self.view.addSubview(button)
+        
+        
+        
     }
 
 
