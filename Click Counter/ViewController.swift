3b8604007a2927210d4180532c4cd8416695cbb9
Paul Campbell <paulcampbell@pcampbellHLlaptop.local>
2016-08-11 21:17:46 -0500

    is += 1 to create a simple counter, this closes #1

--- Click Counter/ViewController.swift
+++ Click Counter/ViewController.swift
@@ -10,28 +10,39 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var numerCount: UILabel!
+    var score = 0
+    @IBOutlet weak var countButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        numerCount.text = String(score)
         // Do any additional setup after loading the view, typically from a nib.
         
         //Label
-        var label = UILabel()
-        label.frame = CGRectMake(150,150,60,60)
-        label.text = "0"
-        
-        self.view.addSubview(label)
-        
-        //Button
-        var button = UIButton()
-        button.frame =  CGRectMake(150,250,60,60)
-        button.setTitle("Click", forState: .Normal)
-        button.setTitleColor(UIColor.blueColor(), forState: .Normal)
-        self.view.addSubview(button)
+//        var label = UILabel()
+//        label.frame = CGRectMake(150,150,60,60)
+//        label.text = "0"
+//        
+//        self.view.addSubview(label)
+//        
+//        //Button
+//        var button = UIButton()
+//        button.frame =  CGRectMake(150,250,60,60)
+//        button.setTitle("Click", forState: .Normal)
+//        button.setTitleColor(UIColor.blueColor(), forState: .Normal)
+//        self.view.addSubview(button)
         
         
         
     }
-
+    @IBAction func increaseCount(sender: UIButton) {
+        score += 1
+        print(score)
+//        number += number
+        numerCount.text = String(score)
+    }
 
 
 
